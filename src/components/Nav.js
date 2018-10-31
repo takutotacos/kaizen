2a1172e2bbc7ae82ad538678f634fd3193fbf231
takutotacos <s.takuto0214@gmail.com>
2018-10-31 17:23:06 -0500

    button format applied

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,18 @@
+import '../stylesheets/button.scss'
+
 let React = require('react');
 let NavLink = require('react-router-dom').NavLink;
 
 let Nav = () => {
   return (
     <div className={'flex'}>
-      <NavLink exact activeClassName={'active'} to={'/'}>Home</NavLink>
-      <NavLink exact activeClassName={'active'} to={'/tasks'} className={'margin-l-s'}>Task</NavLink>
-      <NavLink exact activeClassName={'active'} to={'/'} className={'margin-l-s'}>Day</NavLink>
-      <NavLink exact activeClassName={'active'} to={'/'} className={'margin-l-s'}>Time Frame</NavLink>
+      <NavLink exact activeClassName={'active'} to={'/'} className={'button'}>Home</NavLink>
+      <NavLink exact activeClassName={'active'} to={'/task'} className={'margin-l-s button'}>New Task!</NavLink>
+      <NavLink exact activeClassName={'active'} to={'/tasks'} className={'margin-l-s button'}>Tasks</NavLink>
+      <NavLink exact activeClassName={'active'} to={'/'} className={'margin-l-s button'}>Day</NavLink>
+      <NavLink exact activeClassName={'active'} to={'/'} className={'margin-l-s button'}>Time Frame</NavLink>
     </div>
   )
 }
 
-module.exports = Nav;
+export default Nav;
